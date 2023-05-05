7157b321ee6d9b710216efb5684e1dded7af8b9e
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-05 06:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchAllData() {
+  const startTime = performance.now();
+
+  Promise.all(apiUrls.map(url => fetch(url)))
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+
+      const output = document.getElementById("output-all");
+      output.innerText = `${timeTaken} ms`;
+    })
+    .catch(error => console.error(error));
+}
+
+// Function to fetch data using Promise.any
+function fetchAnyData() {
+  const startTime = performance.now();
+
+  Promise.any(apiUrls.map(url => fetch(url).then(response => response.json())))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+
+      const output = document.getElementById("output-any");
+      output.innerText = `${timeTaken} ms`;
+    })
+    .catch(error => console.error(error));
+}
+
+// Call the functions when the page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  fetchAllData();
+  fetchAnyData();
+});
